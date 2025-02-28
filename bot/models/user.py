faf1bc5b259f3a400f6ec9927632a973ac5c86f7
Yashnarbek <yashnarbeksattorov15@gmail.com>
2025-02-28 16:26:19 +0500

    model changed1

--- bot/models/user.py
+++ bot/models/user.py
@@ -1,38 +1,42 @@
 class User:
-    def __init__(self, fullname, time, vol_id, user_photo, chat_id, language, sheet_id, all_data, sheet_name):
+    def __init__(self, number, fullname, age, work_place, email, hudud, direction, user_photo, offers, language):
+        self._number = number
         self._fullname = fullname
-        self._time = time
-        self._vol_id = vol_id
+        self._age = age
+        self._work_place = work_place
+        self._email = email
         self._user_photo = user_photo
-        self._chat_id = chat_id
+        self._hudud = hudud
+        self._direction = direction
+        self._offers = offers
         self._language = language
-        self._sheet_id = sheet_id
-        self._all_data = all_data
-        self._sheet_name = sheet_name
 
     def get_fullname(self):
         return self._fullname
 
-    def get_time(self):
-        return self._time
+    def get_number(self):
+        return self._number
 
-    def get_vol_id(self):
-        return self._vol_id
+    def get_age(self):
+        return self._age
 
     def get_user_photo(self):
         return self._user_photo
 
-    def get_chat_id(self):
-        return self._chat_id
+    def get_work_place(self):
+        return self._work_place
 
     def get_language(self):
         return self._language
 
-    def get_sheet_id(self):
-        return self._sheet_id
+    def get_offers(self):
+        return self._offers
 
-    def get_datas(self):
-        return self._all_data
-
-    def get_sheet_name(self):
-        return self._sheet_name
+    def get_hudud(self):
+        return self._hudud
+    
+    def get_direction(self):
+        return self._direction
+    
+    def get_email(self):
+        return self._email
